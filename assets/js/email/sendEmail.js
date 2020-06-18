1489ddf46a2e12e2f8236e7723e22c422075ada9
greg <greg.verissimo@gmail.com>
2020-06-18 10:16:56 +0200

    add fetch proxi

--- assets/js/email/sendEmail.js
+++ assets/js/email/sendEmail.js
@@ -13,11 +13,25 @@ function fetchEmail(email) {
 		method: 'POST',
 		body: JSON.stringify(data)
 	};
+	// let options = {
+	// 	method: 'POST',
+	// 	body: JSON.stringify(data),
+	// 	mode: 'cors',
+	// 	headers: {'Access-Control-Allow-Origin':'*'}
+	// };
+	
 	// // Local
 	// let url = "http://localhost:1664/api/email/add";
 
 	// Heroku
-	let url = "https://api-email-greg.herokuapp.com/api/email/add";
+	// let url = "https://api-email-greg.herokuapp.com/api/email/add";
+	let url = "https://cors-anywhere.herokuapp.com/https://api-email-greg.herokuapp.com/api/email/add";
+
+	// fetch("url", {
+	// 	mode: 'cors',
+	// 	headers: {
+	// 	  'Access-Control-Allow-Origin':'*'
+	// 	}
 
 	fetch(url, options).then(
 		function (response) {
